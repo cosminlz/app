03afe3277076d72fed6299f0833d1f1212bb899c
Cosmin <cosmin.lazar.zahacinschi88@gmail.com>
2020-02-23 16:25:59 +0200

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LeftToolbarComponent } from './left-toolbar/left-toolbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LeftToolbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
